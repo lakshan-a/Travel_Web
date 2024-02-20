98732e42d37e1c412936e4d7d83e6ce8ebdd6e93
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-20 20:11:18 +0530

    updated

--- assets/js.js
+++ assets/js.js
@@ -1,3 +1,8 @@
 // Javacript of responsive navigation menu
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click",() =>{
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
