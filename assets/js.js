f165ef8a6cb86688c31838e58db998ea108e529e
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-20 20:15:06 +0530

    updated

--- assets/js.js
+++ assets/js.js
@@ -6,3 +6,20 @@ menuBtn.addEventListener("click",() =>{
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
 });
+
+// Javacript for video slider navigation
+const btns = document.querySelectorAll(".nav-btn");
+
+var sliderNav = function (manul){
+    btns.forEach((btn) => {
+        btn.classList.remove("active")
+    });
+
+    btns[manul].classList.add("active");
+}
+
+btns.forEach((btn,i) => {
+    btn.addEventListener("click",() => {
+        sliderNav(i);
+    });
+});
