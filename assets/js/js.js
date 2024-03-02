9ee79e9e09b23012b9d05b4f01fd0896a062c796
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-03 00:21:20 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -45,3 +45,33 @@ $(window).load(function(){
         }
     });
 });
+
+$('.active-gallery-carousel').owlCarousel({
+    items: 2,
+    // autoplay: 2500,
+    loop: true,
+    margin: 30,
+    nav: true,
+    navText: [ "<img src='./assets/images/cprev.png'>", "<img src='./assets/img/cnext.png'>" ],
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        420: {
+            items: 1
+        },
+        575: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1200: {
+            items: 2
+        },
+        1680: {
+            items: 2
+        }
+    }
+});
