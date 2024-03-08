97f5b23c24128ba478d10d55499c84f50a857649
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-08 22:33:15 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -58,3 +58,50 @@ $(window).load(function(){
     }
 
 })(window.jQuery);
+
+var Mazo = function(){
+    var handleheartBlast = function (){
+        $(".heart").on("click", function() {
+            $(this).toggleClass("heart-blast");
+        });
+    }
+
+
+    var handleSupport = function(){
+        var support = '<a href="https://1.envato.market/qn2QWN" target="_blank" class="bt-buy-now theme-btn"><i class="fas fa-shopping-cart"></i><span>Buy Now</span></a><!-- Go to www.addthis.com/dashboard to customize your tools --><script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b221c5e31b4e54b"></script>';
+        jQuery('body').append(support);
+    }
+
+    /* Function ============ */
+    return {
+        init:function(){
+            handleheartBlast();
+            handleSupport();
+        },
+
+        load:function(){
+            handleRadialProgress();
+        },
+
+        resize:function(){
+            handleFinalCountDown();
+        }
+    }
+
+}();
+
+/* Document.ready Start */
+jQuery(document).ready(function() {
+    'use strict';
+    Mazo.init();
+
+    $('a[data-toggle="tab"]').on('click', function(){
+        // todo remove snippet on bootstrap v4
+        $($(this).attr('href')).show().addClass('show active').siblings().hide();
+    });
+
+    jQuery('.navicon').on('click',function(){
+        $(this).toggleClass('open');
+    });
+
+});
