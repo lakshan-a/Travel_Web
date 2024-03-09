b4cc8bc5f830dca480e7d554f4422658e47957e1
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-09 21:30:10 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,47 +1,47 @@
-/*=============== SHOW MENU ===============*/
+/*/!*=============== SHOW MENU ===============*!/
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
-/* Menu show */
+/!* Menu show *!/
 navToggle.addEventListener('click', () =>{
     navMenu.classList.add('show-menu')
 })
 
-/* Menu hidden */
+/!* Menu hidden *!/
 navClose.addEventListener('click', () =>{
     navMenu.classList.remove('show-menu')
 })
 
-/*=============== SEARCH ===============*/
+/!*=============== SEARCH ===============*!/
 const search = document.getElementById('search'),
     searchBtn = document.getElementById('search-btn'),
     searchClose = document.getElementById('search-close')
 
-/* Search show */
+/!* Search show *!/
 searchBtn.addEventListener('click', () =>{
     search.classList.add('show-search')
 })
 
-/* Search hidden */
+/!* Search hidden *!/
 searchClose.addEventListener('click', () =>{
     search.classList.remove('show-search')
 })
 
-/*=============== LOGIN ===============*/
+/!*=============== LOGIN ===============*!/
 const login = document.getElementById('login'),
     loginBtn = document.getElementById('login-btn'),
     loginClose = document.getElementById('login-close')
 
-/* Login show */
+/!* Login show *!/
 loginBtn.addEventListener('click', () =>{
     login.classList.add('show-login')
 })
 
-/* Login hidden */
+/!* Login hidden *!/
 loginClose.addEventListener('click', () =>{
     login.classList.remove('show-login')
-})
+})*/
 
 const  header =document.querySelector("header");
 
