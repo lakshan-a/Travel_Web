139f913ed449a14838e8f3200a0bb004305de580
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-09 20:09:24 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -13,7 +13,20 @@ navClose.addEventListener('click', () =>{
     navMenu.classList.remove('show-menu')
 })
 
+/*=============== SEARCH ===============*/
+const search = document.getElementById('search'),
+    searchBtn = document.getElementById('search-btn'),
+    searchClose = document.getElementById('search-close')
 
+/* Search show */
+searchBtn.addEventListener('click', () =>{
+    search.classList.add('show-search')
+})
+
+/* Search hidden */
+searchClose.addEventListener('click', () =>{
+    search.classList.remove('show-search')
+})
 
 
 // Javacript of responsive navigation menu
