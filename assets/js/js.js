0be2a09bc01ae50a02b3eae36cc470b769e4f2fc
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-03-11 12:04:09 +0530

    updated

--- assets/js/js.js
+++ assets/js/js.js
@@ -191,6 +191,22 @@ jQuery(document).ready(function() {
 
 });
 
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+});
+
 
 (function($) {
     function onHoverthreeDmovement() {
